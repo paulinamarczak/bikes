5e73a1c62cdb73c279ca857c9b8474d826652345
Paulina Marczak <paulinamarczak@users.noreply.github.com>
2024-07-12 23:14:04 +0000

    added test

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
-from flask import Flask, render_template, send_from_directory, jsonify
+from flask import Flask, render_template, send_from_directory
 import os
 
 app = Flask(__name__)
-
-# Configuration
-FILE_DIRECTORY = 'files'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/files')
-def list_files():
-    files = os.listdir(FILE_DIRECTORY)
-    return jsonify(files)
-
-@app.route('/file/<filename>')
-def get_file(filename):
-    return send_from_directory(FILE_DIRECTORY, filename)
+@app.route('/tiffs/<filename>')
+def serve_tiff(filename):
+    return send_from_directory(os.path.join(app.root_path, 'static/tiffs'), filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port = 5001)
--- static/js/main.js
+++ static/js/main.js
@@ -7,41 +7,55 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// Define the options for the GeoTIFF layer using the RGB renderer
+var options = {
+    renderer: new L.LeafletGeotiff.rgb({
+        rBand: 0,
+        gBand: 1,
+        bBand: 2,
+        alphaBand: 0,
+        transpValue: 0
+    })
+};
 
 // Create GeoTIFF layers
-var layer1 = new L.LeafletGeotiff('/tiffs/alternate_4326.tif', {
-    renderer: new L.LeafletGeotiff.Plotty({
-        displayMin: 0,
-        displayMax: 255,
-        colorScale: 'viridis'
-    })
-});
+var layer1 = L.leafletGeotiff('/tiffs/alternate_4326.tif', options);
 
 var layer2 = new L.LeafletGeotiff('/tiffs/constance_4326.tif', {
-    renderer: new L.LeafletGeotiff.Plotty({
-        displayMin: 0,
-        displayMax: 255,
-        colorScale: 'viridis'
+    renderer: new L.LeafletGeotiff.rgb({
+        rBand: 0,
+        // Optional, band index to use as G-band
+        gBand: 1,
+        // Optional, band index to use as B-band
+        bBand: 2,
+        // band index to use as alpha-band
+        // NOTE: this can also be used in combination with transpValue, then referring to a
+        // color band specifying a fixed value to be interpreted as transparent
+        alphaBand: 0,
+        // for all values equal to transpValue in the band alphaBand, the newly created alpha
+        // channel will be set to 0 (transparent), all other pixel values will result in alpha 255 (opaque)
+        transpValue: 0,
+        renderer: renderer,
     })
 });
 
-// Initially hide the layers
-map.removeLayer(layer1);
-map.removeLayer(layer2);
+// // Initially hide the layers
+// map.removeLayer(layer1);
+// map.removeLayer(layer2);
 
-// Layer control logic
-document.getElementById('layer1').addEventListener('change', function() {
-    if (this.checked) {
-        layer1.addTo(map);
-    } else {
-        map.removeLayer(layer1);
-    }
-});
+// // Layer control logic
+// document.getElementById('layer1').addEventListener('change', function() {
+//     if (this.checked) {
+//         layer1.addTo(map);
+//     } else {
+//         map.removeLayer(layer1);
+//     }
+// });
 
-document.getElementById('layer2').addEventListener('change', function() {
-    if (this.checked) {
-        layer2.addTo(map);
-    } else {
-        map.removeLayer(layer2);
-    }
-});
+// document.getElementById('layer2').addEventListener('change', function() {
+//     if (this.checked) {
+//         layer2.addTo(map);
+//     } else {
+//         map.removeLayer(layer2);
+//     }
+// });
