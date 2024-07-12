d53f0cb5b768469ff27bd7b954ef85cdd1ffe78b
Paulina Marczak <paulinamarczak@users.noreply.github.com>
2024-07-12 21:21:08 +0000

    ok

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, jsonify
 import os
 
 app = Flask(__name__)
+
+# Configuration
+FILE_DIRECTORY = 'files'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/tiff/<filename>')
-def serve_tiff(filename):
-    return send_from_directory(os.path.join(app.root_path, 'static', 'tiffs'), filename)
+@app.route('/files')
+def list_files():
+    files = os.listdir(FILE_DIRECTORY)
+    return jsonify(files)
+
+@app.route('/file/<filename>')
+def get_file(filename):
+    return send_from_directory(FILE_DIRECTORY, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
